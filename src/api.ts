ccaf4f55ea4b065e7724290e24b7620249fdc35b
Gab-Ahrens <gab.ahrens@gmail.com>
2025-03-22 02:42:14 +0100

    fix season

--- src/api.ts
+++ src/api.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const USE_MOCK = process.env.USE_MOCK_DATA === 'true';
 const API_BASE_URL = 'https://api-football-v1.p.rapidapi.com/v3';
 const TEAM_ID = 119;
-const SEASON = 2024;
+const SEASON = 2025;
 
 export async function fetchNextMatch() {
   if (USE_MOCK) {
